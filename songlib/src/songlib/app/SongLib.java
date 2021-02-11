fc5d36439909830ab5836bd7df1477f9b01f3d31
Sruthi Soorian <sruthi.soorian@gmail.com>
2021-02-10 22:47:26 -0500

    First Commit Scaffold Stuff

---
+++ songlib/src/songlib/app/SongLib.java
@@ -0,0 +1,20 @@
+package songlib.app;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class SongLib extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		launch(args);
+
+	}
+
+}
---
+++ songlib/src/songlib/view/songlibController.java
@@ -0,0 +1,11 @@
+package songlib.view;
+
+import javafx.event.ActionEvent;
+
+public class songlibController {
+	
+	public void convert(ActionEvent e) {
+		
+	}
+
+}
