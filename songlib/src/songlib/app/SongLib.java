ca04b6e86d673d62fca8993768324c8ba83e786f
Sruthi Soorian <sruthi.soorian@gmail.com>
2021-02-12 17:26:10 -0500

    Need to fix error within FXML for launch. Error occurs on SongLib.java @ line 15

--- songlib/src/songlib/app/SongLib.java
+++ songlib/src/songlib/app/SongLib.java
@@ -1,6 +1,9 @@
 package songlib.app;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class SongLib extends Application {
@@ -8,6 +11,14 @@ public class SongLib extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		// TODO Auto-generated method stub
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(getClass().getResource("/songlib/view/songlib.fxml"));
+		GridPane root = (GridPane)loader.load();
+		Scene scene = new Scene(root);
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("Song-Library");
+		primaryStage.setResizable(false);
+		primaryStage.show();
 
 	}
 
