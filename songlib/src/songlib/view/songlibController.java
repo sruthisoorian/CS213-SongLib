3d08c9777e21b3806b8e1c3e22344b81cdbbd170
Sruthi Soorian <sruthi.soorian@gmail.com>
2021-02-13 13:47:17 -0500

    Edited controller to import fxml components. Added controller line to fxml file. Currently crying.

--- songlib/src/songlib/view/songlibController.java
+++ songlib/src/songlib/view/songlibController.java
@@ -1,11 +1,26 @@
 package songlib.view;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 
 public class songlibController {
-	
-	public void convert(ActionEvent e) {
-		
-	}
+	@FXML Button addB;
+	@FXML Button editB;
+	@FXML TextField songTF;
+	@FXML TextField albumTF;
+	@FXML TextField artistTF;
+	@FXML TextField yearTF;
+	@FXML Button deleteB;
+	@FXML Button cancelB;
+	@FXML Button confirmB;
+	@FXML Label songL;
+	@FXML Label artistL;
+	@FXML Label albumL;
+	@FXML Label yearL;
+	@FXML ListView<Integer> songLV;
 
 }
