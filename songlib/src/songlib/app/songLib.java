525f3b3c3b9433c728b43bfb4831cde37dc96686
bria6 <bria6862@gmail.com>
2021-02-13 18:05:40 -0500

    songLib.java workingg

---
+++ songlib/src/songlib/app/songLib.java
@@ -0,0 +1,38 @@
+package songlib.app;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import songlib.view.songlibController;
+
+public class songLib extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(getClass().getResource("/songlib/view/songlib.fxml"));
+		VBox root = (VBox)loader.load();
+		
+		songlibController songlibC = loader.getController();
+		songlibC.start();
+		
+		Scene scene = new Scene(root);
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("Song-Library");
+		primaryStage.setResizable(false);
+		primaryStage.show();
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		launch(args);
+
+	}
+
+}
