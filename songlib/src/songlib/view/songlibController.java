a117639d3be02fe590b20432230e9beeb6ddaa28
bria6 <bria6862@gmail.com>
2021-02-13 17:59:54 -0500

    controller, adds a couple songs on start

--- songlib/src/songlib/view/songlibController.java
+++ songlib/src/songlib/view/songlibController.java
@@ -1,11 +1,13 @@
 package songlib.view;
 
-import javafx.event.ActionEvent;
+//import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 public class songlibController {
 	@FXML Button addB;
@@ -21,6 +23,13 @@ public class songlibController {
 	@FXML Label artistL;
 	@FXML Label albumL;
 	@FXML Label yearL;
-	@FXML ListView<Integer> songLV;
+	@FXML ListView<String> songLV;
+	
+	private ObservableList<String> obsList;
+	
+	public void start () {
+		obsList = FXCollections.observableArrayList("nasty - Ariana Grande",  "my future - Billie Eilish");
+		songLV.setItems(obsList);
+	}
 
 }
